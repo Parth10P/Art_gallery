186b3ac009e538f2090e2721cc640635fd816427
Stark <sahil.gen9@gmail.com>
2024-12-10 02:10:51 +0530

    Cheetahh

--- about.js
+++ about.js
@@ -182,4 +182,14 @@ parth.addEventListener("mouseleave",function(){
         scale:1,
         duration:0.4
     })
+    
+})
+var main = document.querySelector("body")
+var cursor = document.querySelector("#cursor")
+main.addEventListener("mousemove",function(dets){
+    gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y,
+        ease:"circ.out"
+    })
 })
