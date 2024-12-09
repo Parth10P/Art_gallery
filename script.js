187907ccc9cc4d5763dfb99773cbebbf13eeec75
AN Pavan Sai <student102006@gmail.com>
2024-12-10 00:48:03 +0530

    linked location.html in the location tab

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 
 var nav = gsap.timeline()
 
-nav.from("h2",{
-    y:-30,
-    opacity:0,
-    duration:0.8,
-    delay:0.5,
+nav.from("h2", {
+    y: -30,
+    opacity: 0,
+    duration: 0.8,
+    delay: 0.5,
 })
 
-nav.from("h4",{
-    y:-30,
-    opacity:0,
-    duration:0.8,
-    stagger:0.4,
+nav.from("h4", {
+    y: -30,
+    opacity: 0,
+    duration: 0.8,
+    stagger: 0.4,
 })
-nav.from(".center",{
-    y:20,
-    opacity:0,
-    duration:0.8,
-    scale:0.2,
+nav.from(".center", {
+    y: 20,
+    opacity: 0,
+    duration: 0.8,
+    scale: 0.2,
 })
